03d3e87801e240a9308a43462ee91699ae84f63d
Fergus Baker <fergusbkr@gmail.com>
2022-04-22 19:25:38 +0100

    circular orbit smoke tests

---
+++ test/smoke-tests/circular-orbits.jl
@@ -0,0 +1,30 @@
+# smoke test to make sure circular orbits work
+using Test, Gradus
+
+@testset "pointfunctions" begin
+
+    @testset "solve_equitorial_circular_orbit" begin
+        # only implemented for the BoyerLindquist metrics at the moment
+        # expected is sum of the circular orbit vϕ
+        # last updated: 22 Apr 2022
+        r_range = 6.0:0.5:10.0
+        for (m, expected) in [
+            (BoyerLindquistAD(M = 1.0, a = 0.0), 0.5432533297869712),
+            (BoyerLindquistAD(M = 1.0, a = 1.0), 0.5016710246454921),
+            (BoyerLindquistAD(M = 1.0, a = -1.0), 0.5993458160081419),
+            (JohannsenAD(M = 1.0, a = 1.0, α22 = 1.0), 0.4980454719932759),
+        ]
+            vϕs = solve_equitorial_circular_orbit(m, r_range)
+            @test isapprox(sum(vϕs), expected, atol = 1e-6, rtol = 0.0)
+        end
+    end
+
+    @testset "trace_equitorial_circular_orbit" begin
+        m = BoyerLindquistAD(M = 1.0, a = 0.0)
+        Gradus.isco(m)
+        r_range = 6.0:0.1:10.0
+        simsols = @time trace_equitorial_circular_orbit(m, r_range)
+        # smoke test passed
+        @test true
+    end
+end
