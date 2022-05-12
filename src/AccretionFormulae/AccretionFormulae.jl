598429bb9acb450520a03744b564288e04245a9b
Fergus Baker <fergusbkr@gmail.com>
2022-05-12 14:19:32 +0100

    interpolating plunging velocities

--- src/AccretionFormulae/AccretionFormulae.jl
+++ src/AccretionFormulae/AccretionFormulae.jl
@@ -8,10 +8,16 @@ using ..Rendering: PointFunction
 using Optim: optimize, minimizer, GoldenSection, Brent
 using DocStringExtensions
 using StaticArrays
+using Interpolations
 
 include("redshift.jl")
 include("orbit-discovery.jl")
+include("orbit-interpolations.jl")
 
-export solve_equitorial_circular_orbit, trace_equitorial_circular_orbit, CircularOrbits
+export solve_equitorial_circular_orbit,
+    trace_equitorial_circular_orbit,
+    CircularOrbits,
+    interpolate_plunging_velocities,
+    PlungingInterpolation
 
 end # module
