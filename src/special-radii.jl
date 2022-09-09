e0e1ff6988f9974f5a755172e093c174f6416148
Fergus Baker <fergusbkr@gmail.com>
2022-09-09 01:45:44 +0100

    added a function for plotting event horizons

--- src/special-radii.jl
+++ src/special-radii.jl
@@ -75,3 +75,59 @@ Event horizon radius, often equivalent to [`GradusBase.inner_radius`](@ref), how
 distinct, such that the latter may still be an arbitrary chart cutoff.
 """
 r_s(m::AbstractMetricParams{T}) where {T} = error("Not implemented for $(typeof(m)).")
+
+"""
+    event_horizon(m::AbstractMetricParams; select = last, resolution = 100, θε = 1e-7, rmax = 5.0)
+
+Utility function for helping plot an event horizon shape. Returns a tuple containing the `r`
+and `θ` vectors that solve
+
+```math
+    g_{t\\phi}^2 - g_{tt} g_{\\phi \\phi} = 0.
+```
+
+A `NaN` value in the `r` vector indicates no solution for that particular ``\\theta``, i.e.
+that the metric describes a naked singularity.
+
+Often the equation will have multiple roots, in which case the keyword argument `select` may be
+assigned to select the desired root.
+"""
+event_horizon(m::AbstractMetricParams{T}; kwargs...) where {T} =
+    error("Not implemented for $(typeof(m)).")
+
+function _event_horizon_condition(m, r, θ)
+    g = metric_components(m, (r, θ))
+    g[5]^2 - g[1] * g[4]
+end
+
+function event_horizon(
+    m::AbstractAutoDiffStaticAxisSymmetricParams{T};
+    select = maximum,
+    resolution::Int = 100,
+    θε::T = T(1e-7),
+    rmax = 5.0,
+) where {T}
+    θs = range(θε, 2π - θε, resolution)
+    rs = map(θs) do θ
+        f(r) = _event_horizon_condition(m, r, θ)
+        r = Roots.find_zeros(f, 0.0, rmax)
+        if isempty(r)
+            push!(r, NaN)
+        end
+        select(r)
+    end
+    rs, θs
+end
+
+function is_naked_singularity(
+    m::AbstractAutoDiffStaticAxisSymmetricParams{T};
+    resolution::Int = 100,
+    θε::T = T(1e-7),
+    rmax = 5.0,
+) where {T}
+    any(range(θε, 2π - θε, resolution)) do θ
+        f(r) = _event_horizon_condition(m, r, θ)
+        r = Roots.find_zeros(f, 0.0, rmax)
+        isempty(r)
+    end
+end
