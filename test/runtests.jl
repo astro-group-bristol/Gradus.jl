06caa9b10d56cc239b3b44736a49167f3a93d92d
fjebaker <fergusbkr@gmail.com>
2022-09-16 15:29:13 +0100

    quality assurance with aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,6 @@
-using Test, Gradus, StaticArrays
+using Gradus, StaticArrays
+using Test
+using Aqua
 
 include("smoke-tests/rendergeodesics.jl")
 include("smoke-tests/tracegeodesics.jl")
@@ -7,3 +9,8 @@ include("smoke-tests/circular-orbits.jl")
 include("smoke-tests/disc-profiles.jl")
 include("smoke-tests/special-radii.jl")
 include("smoke-tests/cunningham-transfer-functions.jl")
+
+# little bit of aqua
+Aqua.test_undefined_exports(Gradus)
+Aqua.test_unbound_args(Gradus)
+Aqua.test_stale_deps(Gradus)
--- test/smoke-tests/cunningham-transfer-functions.jl
+++ test/smoke-tests/cunningham-transfer-functions.jl
@@ -9,14 +9,8 @@
         [248.95547957677172, 228.40998327542954, 136.93067378322644, 99.95201562448777],
     )
         u = @SVector [0.0, 1000.0, deg2rad(angle), 0.0]
-        ctf = Gradus.cunningham_transfer_function(
-            m,
-            u,
-            d,
-            4.0,
-            2000.0;
-            finite_diff_order = 5,
-        )
+        ctf =
+            Gradus.cunningham_transfer_function(m, u, d, 4.0, 2000.0; finite_diff_order = 5)
         s = sum(ctf.f)
         @test isapprox(s, expected, atol = 1e-2, rtol = 0.0)
     end
@@ -27,14 +21,7 @@
         [234.24090499819158, 259.3365885895835, 266.8322238866137, 271.2289331188517],
     )
         u = @SVector [0.0, 1000.0, deg2rad(30), 0.0]
-        ctf = Gradus.cunningham_transfer_function(
-            m,
-            u,
-            d,
-            r,
-            2000.0;
-            finite_diff_order = 5,
-        )
+        ctf = Gradus.cunningham_transfer_function(m, u, d, r, 2000.0; finite_diff_order = 5)
         s = sum(ctf.f)
         @test isapprox(s, expected, atol = 1e-2, rtol = 0.0)
     end
