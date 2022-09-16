a6d160e65973aa3c99b4c85714e395c81fc0e7ee
fjebaker <fergusbkr@gmail.com>
2022-09-16 15:39:28 +0100

    reduce time spent testing cunningham

--- test/smoke-tests/cunningham-transfer-functions.jl
+++ test/smoke-tests/cunningham-transfer-functions.jl
@@ -6,11 +6,12 @@
     # test for different angles
     for (angle, expected) in zip(
         [3, 35, 74, 85],
-        [248.95547957677172, 228.40998327542954, 136.93067378322644, 99.95201562448777],
+        # values last updated: 16/09/2022 - reduced number of points in test set
+        [49.09620827339265, 45.16844822761859, 27.107307569529434, 19.664521288378978],
     )
         u = @SVector [0.0, 1000.0, deg2rad(angle), 0.0]
         ctf =
-            Gradus.cunningham_transfer_function(m, u, d, 4.0, 2000.0; finite_diff_order = 5)
+            cunningham_transfer_function(m, u, d, 4.0, 2000.0; finite_diff_order = 5, num_points = 200)
         s = sum(ctf.f)
         @test isapprox(s, expected, atol = 1e-2, rtol = 0.0)
     end
@@ -18,10 +19,11 @@
     # different radii
     for (r, expected) in zip(
         [4.0, 7.0, 10.0, 15.0],
-        [234.24090499819158, 259.3365885895835, 266.8322238866137, 271.2289331188517],
+        # values last updated: 16/09/2022 - reduced number of points in test set
+        [46.446844434569144, 51.08146707424876, 52.788771093295686, 53.78540573544522],
     )
         u = @SVector [0.0, 1000.0, deg2rad(30), 0.0]
-        ctf = Gradus.cunningham_transfer_function(m, u, d, r, 2000.0; finite_diff_order = 5)
+        ctf = cunningham_transfer_function(m, u, d, r, 2000.0; finite_diff_order = 5, num_points = 200)
         s = sum(ctf.f)
         @test isapprox(s, expected, atol = 1e-2, rtol = 0.0)
     end
