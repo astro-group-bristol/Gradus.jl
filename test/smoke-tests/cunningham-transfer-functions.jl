5d11852206c64d9d24ff652276dfe3f17f449cdd
fjebaker <fergusbkr@gmail.com>
2022-09-25 15:22:45 +0100

    tests for surrogate disc profile

--- test/smoke-tests/cunningham-transfer-functions.jl
+++ test/smoke-tests/cunningham-transfer-functions.jl
@@ -10,8 +10,15 @@
         [49.09620827339265, 45.16844822761859, 27.107307569529434, 19.664521288378978],
     )
         u = @SVector [0.0, 1000.0, deg2rad(angle), 0.0]
-        ctf =
-            cunningham_transfer_function(m, u, d, 4.0, 2000.0; finite_diff_order = 5, num_points = 200)
+        ctf = cunningham_transfer_function(
+            m,
+            u,
+            d,
+            4.0,
+            2000.0;
+            finite_diff_order = 5,
+            num_points = 200,
+        )
         s = sum(ctf.f)
         @test isapprox(s, expected, atol = 1e-2, rtol = 0.0)
     end
@@ -23,7 +30,15 @@
         [46.446844434569144, 51.08146707424876, 52.788771093295686, 53.78540573544522],
     )
         u = @SVector [0.0, 1000.0, deg2rad(30), 0.0]
-        ctf = cunningham_transfer_function(m, u, d, r, 2000.0; finite_diff_order = 5, num_points = 200)
+        ctf = cunningham_transfer_function(
+            m,
+            u,
+            d,
+            r,
+            2000.0;
+            finite_diff_order = 5,
+            num_points = 200,
+        )
         s = sum(ctf.f)
         @test isapprox(s, expected, atol = 1e-2, rtol = 0.0)
     end
