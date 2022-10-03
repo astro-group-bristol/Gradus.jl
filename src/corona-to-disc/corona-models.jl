57925602ae055fc3026e2aa76b0b52d99f4db2c5
fjebaker <fergusbkr@gmail.com>
2022-10-03 22:39:51 +0100

    metric-generic isotropic angle mapping

--- src/corona-to-disc/corona-models.jl
+++ src/corona-to-disc/corona-models.jl
@@ -1,5 +1,62 @@
-sample_position(m::AbstractMetricParams{T}, model::AbstractCoronaModel{T}, N) where {T} =
+# todo: this should take into account position
+function source_velocity(::AbstractMetricParams, model::AbstractCoronaModel)
     error("Not implemented for $(typeof(model)).")
+end
+
+function sample_position(
+    ::AbstractMetricParams{T},
+    model::AbstractCoronaModel{T},
+    N,
+) where {T}
+    error("Not implemented for $(typeof(model)).")
+end
+
+"""
+    sample_velocity(
+        m::AbstractMetricParams, 
+        model::AbstractCoronaModel, 
+        sampler::AbstractDirectionSampler, 
+        us, 
+        N
+    )
+
+Sample `N` initial (un-normalised) velocities in the local coordinates of the `model`, according to
+the [`AbstractDirectionSampler`](@ref) algorithm and domain, given initial 4-vector positions `us`.
+
+This function is metric generic and is implemented in the following way:
+- Sample angles ``\\alpha`` and ``\\beta`` in the local sky.
+- Map these angles to a spherical polar vector (interally first to Cartesian and then using a Jacobian
+transformation to spherical polar).
+- Calculate the local tetrad given the source velocity at `u = us[i]`.
+- Use this tetrad to map the local vector to the global coordinates.
+"""
+function sample_velocity(
+    m::AbstractMetricParams{T},
+    model::AbstractCoronaModel{T},
+    sampler::AbstractDirectionSampler,
+    us,
+    N,
+) where {T}
+    u_source = source_velocity(m, model)
+    @inbounds map(1:N) do index
+        # todo: sampler should have proper iterator interface
+        i = geti(sampler, index, N)
+        θ, ϕ = sample_angles(sampler, i, N)
+
+        u = us[index]
+        # multiply by -1 for consitency with LowerHemisphere()
+        hat = -1 * _cart_local_direction(θ, ϕ)
+        J = _cart_to_spher_jacobian(u[3], u[4])
+        (r, t, p) = J * hat
+
+        v = @SVector [T(0.0), r, t, p]
+
+        basis = tetradframe(m, u, u_source)
+        B = reduce(hcat, basis)
+        B * v
+    end
+end
+
 
 @with_kw struct LampPostModel{T} <: AbstractCoronaModel{T}
     @deftype T
@@ -11,26 +68,6 @@ end
 function sample_position(::AbstractMetricParams{T}, model::LampPostModel{T}, N) where {T}
     u = @SVector [T(0.0), model.h, model.θ, model.ϕ]
     fill(u, N)
-end
-
-function sample_velocity(
-    m::AbstractMetricParams{T},
-    ::AbstractCoronaModel{T},
-    sampler::AbstractDirectionSampler,
-    us,
-    N,
-) where {T}
-    map(1:N) do index
-        # todo: sampler should have proper iterator interface
-        i = geti(sampler, index, N)
-        θ, ϕ = sample_angles(sampler, i, N)
-        r, t, p = vector_to_local_sky(m, us[index], θ, ϕ)
-        @SVector [T(0.0), r, t, p]
-    end
-end
-
-function source_velocity(m::AbstractMetricParams, model::AbstractCoronaModel)
-    error("Not implemented for $(typeof(model)).")
 end
 
 function source_velocity(m::AbstractMetricParams, model::LampPostModel)
