66da1aeadfde3bd9bae7fc68edc53365c2ca49db
Fergus Baker <fergusbkr@gmail.com>
2022-10-25 11:12:38 +0100

    smoke test for cunningham line profile

---
+++ test/smoke-tests/line-profiles.jl
@@ -0,0 +1,22 @@
+
+@testset "line-profiles" begin
+    d = GeometricThinDisc(0.0, 300.0, π / 2)
+    u = @SVector [0.0, 1000.0, deg2rad(40), 0.0]
+    m = BoyerLindquistAD(M = 1.0, a = 0.998)
+
+    @testset "cunningham" begin
+        x = range(0.1, 1.2, 20)
+        bins, lp = lineprofile(
+            (re) -> re^(-3),
+            m,
+            u,
+            d;
+            num_points = 30,
+            bins = x,
+            num_re = 35,
+            max_re = 50,
+            finite_diff_order = 7,
+        )
+        @test isapprox(15.685722, sum(lp), atol = 1e-4)
+    end
+end
