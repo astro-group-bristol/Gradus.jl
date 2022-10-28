2eb9d8247c780f63a7651004b9f38d858466105b
Fergus Baker <fergusbkr@gmail.com>
2022-10-28 15:24:54 +0100

    working integration but poorly

--- src/line-profiles.jl
+++ src/line-profiles.jl
@@ -19,26 +19,39 @@ function _change_interval(f, a, b)
     (α, x -> f(α * x + β))
 end
 
-function lineprofile(
-    ::CunninghamLineProfile,
-    m::AbstractMetricParams{T},
-    u,
-    d::AbstractAccretionGeometry,
-    ε;
-    num_points = 100,
-    min_re = isco(m),
-    max_re = 20,
-    num_re = 100,
-    bins = range(0.0, 1.5, 100),
-    kwargs...,
-) where {T}
-    # Gauss-Lobatto to include [-1, 1]
-    radii, w = gausslobatto(num_re)
+function _cunningham_line_profile_integrand(ictb)
+    gs -> begin
+        if !Gradus._gs_in_limits(ictb, gs)
+            0.0
+        else
+            gmin, gmax = ictb.g_extrema
+            g = gstar_to_g(gs, gmin, gmax)
+            w = g^3 / √(gs * (1 - gs))
+            f = ictb.lower(gs) + ictb.upper(gs)
+            w * f / (gmax - gmin)
+        end
+    end
+end
 
+function _calculate_interpolated_transfer_branches(
+    m,
+    u,
+    d,
+    radii;
+    num_points = 100,
+    verbose = false,
+    offset = 1e-7,
+    kwargs...,
+)
+    # pre-allocate arrays
     αs = zeros(T, num_points)
     βs = zeros(T, num_points)
     Js = zeros(T, num_points)
-    f =
+
+    progress_bar = init_progress_bar("Transfer functions:", length(radii), verbose)
+
+    # IILF for calculating the interpolated branches
+    𝔉 =
         rₑ -> begin
             # this feels like such a bad practice
             # calling GC on young objects to clear the temporarily allocated memory
@@ -53,27 +66,98 @@ function lineprofile(
                 d,
                 rₑ,
                 2000.0;
-                offset_max = rₑ + 20.0,
+                offset_max = 2rₑ + 20.0,
                 kwargs...,
             )
-            Gradus._interpolate_branches(ctf)
+            ProgressMeter.next!(progress_bar)
+            _interpolate_branches(ctf; offset = offset)
         end
 
-    α, 𝔉 = _change_interval(f, min_re, max_re)
-    ictbs = map(𝔉, radii)
+    # calculate interpolated transfer functions for each emission radius
+    map(𝔉, radii)
+end
 
-    bin_extrema = extrema(bins)
-    y = map(bins) do g
-        α * sum(
-            ((i, ictb),) -> begin
-                w[i] *
-                ε(ictb.radius) *
-                _integrate_tranfer_function_branches(ictb, g, bin_extrema...)
-            end,
-            enumerate(ictbs),
-        )
+function lineprofile(
+    ::CunninghamLineProfile,
+    m::AbstractMetricParams{T},
+    u,
+    d::AbstractAccretionGeometry,
+    ε;
+    num_points = 100,
+    min_re = isco(m) + 1e-2, # delta to avoid numerical instabilities
+    max_re = 20,
+    num_re = 100,
+    bins = range(0.0, 1.5, 100),
+    verbose = false,
+    offset = 1e-7,
+    kwargs...,
+) where {T}
+    # this is just a placeholder: desire a distribution that favours
+    # small radii over large radii, and this one does that quite well
+    # radii here are emission radii rₑ
+    radii = exp.(range(log(1), log(1000), num_re))
+    _max_radii = maximum(radii)
+    # rescale inplace
+    @. radii = (radii / _max_radii) * (max_re - min_re) * min_re
+
+    ictbs = _calculate_interpolated_transfer_branches(
+        m,
+        u,
+        d,
+        radii;
+        num_points = num_points,
+        verbose = verbose,
+        offset = offset,
+        kwargs...,
+    )
+
+    integrate_line_profile(ε, ictbs, bins)
+end
+
+function integrate_line_profile(
+    ε,
+    ictbs::Vector{<:InterpolatedCunninghamTransferBranches},
+    bins,
+)
+    # global min and max
+    ggmin = maximum(i -> first(i.g_limits), ictbs)
+    ggmax = minimum(i -> last(i.g_limits), ictbs)
+
+    # radii = map(i -> i.radius, ictbs)
+    # y = map(eachindex(@view(bins[1:end-1]))) do index
+    #     interpolant = map()
+    # end
+
+    # integrate for each energy bin
+    ℐ =
+        index -> @inbounds begin
+            bin_low = bins[index]
+            bin_high = bins[index+1]
+
+            𝒮 = ictb -> begin
+                S = _cunningham_line_profile_integrand(ictb)
+                Sg = g -> begin
+                    gs = gstar(g, ictb.g_extrema...)
+                    if ggmin < gs < ggmax
+                        S(gs)
+                    else
+                        0.0
+                    end
+                end
+                res, _ = quadgk(Sg, bin_low, bin_high)
+                res * ictb.radius * ε(ictb.radius)
+            end
+
+            DataInterpolations.LinearInterpolation(map(𝒮, ictbs), radii)
+        end
+
+    interpolants = map(ℐ, eachindex(@view(bins[1:end-1])))
+
+    r_low, r_high = extrema(radii)
+    y = map(interpolants) do f
+        res, _ = quadgk(r -> f(r), r_low, r_high)
+        res
     end
-
     (bins, y)
 end
 
