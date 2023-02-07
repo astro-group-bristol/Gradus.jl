b9b5c708b60e73797dfbe4ce3673c6bae40c5488
fjebaker <fergusbkr@gmail.com>
2023-02-07 16:25:30 +0000

    feat: binned lineprofiles

--- src/line-profiles.jl
+++ src/line-profiles.jl
@@ -2,6 +2,11 @@ abstract type AbstractLineProfileAlgorithm end
 struct CunninghamLineProfile <: AbstractLineProfileAlgorithm end
 struct BinnedLineProfile <: AbstractLineProfileAlgorithm end
 
+@inline function lineprofile(m::AbstractMetricParams, u, d::AbstractAccretionGeometry; kwargs...)
+    bins = collect(range(0.1, 1.3, 120))
+    lineprofile(bins, r -> r^-3, m, u, d; kwargs...)
+end
+
 @inline function lineprofile(
     bins,
     ε::Function,
@@ -17,7 +22,7 @@ end
 function lineprofile(
     bins,
     ε::Function,
-    m::AbstractMetricParams{T},
+    m::AbstractMetricParams,
     u,
     d::AbstractAccretionGeometry,
     ::CunninghamLineProfile,
@@ -29,11 +34,40 @@ function lineprofile(
     Ng✶ = 67,
     h = 2e-8,
     kwargs...,
-) where {T}
+)
     radii = Grids._inverse_grid(minrₑ, maxrₑ, numrₑ)
     itfs = interpolated_transfer_branches(m, u, d, radii; verbose = verbose, kwargs...)
     flux = integrate_drdg✶(ε, itfs, radii, bins; h = h, Ng✶ = Ng✶)
     bins, flux
+end
+
+function lineprofile(
+    bins,
+    ε::Function,
+    m::AbstractMetricParams,
+    u,
+    d::AbstractAccretionGeometry,
+    ::BinnedLineProfile;
+    plane = PolarPlane(GeometricGrid(); Nr = 250, Nθ = 1300, r_max = 50.0),
+    λ_max = 2 * u[2],
+    redshift_pf = ConstPointFunctions.redshift(m, u),
+    kwargs...,
+)
+    simsols = tracegeodesics(m, u, plane, d, (0.0, λ_max); save_on = false)
+    gps = process_solution.(m, simsols.u)
+
+    I = [i.status == StatusCodes.IntersectedWithGeometry for i in gps]
+    points = @views gps[I]
+    areas = unnormalized_areas(plane)[I]
+
+    # calculate physical flux
+    g = redshift_pf.(m, points, λ_max)
+    r = [p.u2[2] for p in points]
+
+    f = @. ε(r) * g^3 * areas
+    # bin
+    F = bucket(Simple(), g, f, bins)
+    bins, F ./ sum(F)
 end
 
 export AbstractLineProfileAlgorithm, BinnedLineProfile, CunninghamLineProfile, lineprofile
