07ac5ffc43ae79488d958a7f37565f2bff1ac00d
fjebaker <fergusbkr@gmail.com>
2023-02-07 16:26:43 +0000

    test: slightly better inference tests

--- test/integration/test-inference.jl
+++ test/integration/test-inference.jl
@@ -2,37 +2,29 @@ using Test
 using Gradus
 using StaticArrays
 
+include("../utils.jl")
+
 m = KerrMetric()
 u = @SVector [0.0, 1e3, π / 2, 0.0]
 v = @SVector [0.0, 1.0, 0.0, 0.0]
 
 # single vector for u and v
-info = code_typed(tracegeodesics, (typeof(m), typeof(u), typeof(v), Tuple{Float64,Float64}))
-info = first(info)
-@test isconcretetype(info.second)
+rtype = returntype(tracegeodesics, m, u, v, (0.0, 2000.0))
+@test isconcretetype(rtype)
 
 # multiple vectors for u and v
 u = [u, u]
 v = [v, v]
-info = code_typed(tracegeodesics, (typeof(m), typeof(u), typeof(v), Tuple{Float64,Float64}))
-info = first(info)
-@test isconcretetype(info.second)
+rtype = returntype(tracegeodesics, m, u, v, (0.0, 2000.0))
+@test isconcretetype(rtype)
 
 # v is a velocity function
 u = @SVector [0.0, 1e3, π / 2, 0.0]
 vfunc = (i) -> @SVector [0.0, 1.0, 0.0, 0.0]
-info = code_typed(
-    (args...) -> tracegeodesics(args...; trajectories = 10),
-    (typeof(m), typeof(u), typeof(vfunc), Tuple{Float64,Float64}),
-)
-info = first(info)
-@test isconcretetype(info.second)
+rtype = returntype((args...) -> tracegeodesics(args...; trajectories = 10), m, u, vfunc, (0.0, 2000.0))
+@test isconcretetype(rtype)
 
 # v is a plane
 plane = PolarPlane(LinearGrid())
-info = code_typed(
-    tracegeodesics,
-    (typeof(m), typeof(u), typeof(plane), Tuple{Float64,Float64}),
-)
-info = first(info)
-@test isconcretetype(info.second)
+rtype = returntype(tracegeodesics, m, u, plane, (0.0, 2000.0))
+@test isconcretetype(rtype)
--- test/utils.jl
+++ test/utils.jl
@@ -2,3 +2,9 @@ function count_inner_boundary(m, simsols)
     points = process_solution.(m, simsols.u)
     count(i -> i.status == StatusCodes.WithinInnerBoundary, points)
 end
+
+function returntype(f, args...)
+    T = map(typeof, args)
+    info = code_typed(f, T)
+    first(info).second
+end
