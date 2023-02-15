c6ec0d9efb3f13a5e1a7970cf3c71e44c115c693
fjebaker <fergusbkr@gmail.com>
2023-02-15 19:30:06 +0000

    feat: export svectors

--- src/Gradus.jl
+++ src/Gradus.jl
@@ -81,6 +81,9 @@ export AbstractMetricParams,
     AbstractIntegrationParameters,
     IntegrationParameters
 
+# export static arrays too
+export SVector, @SVector
+
 """
     abstract type AbstractPointFunction
 
