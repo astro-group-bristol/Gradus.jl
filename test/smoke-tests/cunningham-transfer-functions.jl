26f377188bc46c3af7d810f5599afec98ada85dd
fjebaker <fergusbkr@gmail.com>
2023-04-24 15:08:48 +0100

    test: updated cunningham transfer function values

--- test/smoke-tests/cunningham-transfer-functions.jl
+++ test/smoke-tests/cunningham-transfer-functions.jl
@@ -9,8 +9,8 @@ using StaticArrays
     # test for different angles
     for (angle, expected) in zip(
         [3, 35, 74, 85],
-        # values last updated: 12/11/2022 - use means
-        [0.2495227986998514, 0.22911943722172406, 0.13792349421541406, 0.10360637290617522],
+        # values last updated: 24/04/2023
+        [0.24325323183855346, 0.21641300042754005, 0.12129760540199881, 0.07809023014473473],
     )
         u = @SVector [0.0, 1000.0, deg2rad(angle), 0.0]
         ctf = cunningham_transfer_function(m, u, d, 4.0)
@@ -21,8 +21,8 @@ using StaticArrays
     # different radii
     for (r, expected) in zip(
         [4.0, 7.0, 10.0, 15.0],
-        # values last updated: 12/11/2022 - use means
-        [0.23492490558931373, 0.2599801835844643, 0.26738568957410025, 0.2718492609857089],
+        # values last updated: 24/04/2023
+        [0.22097479890817756, 0.2599801835844643, 0.26738568957410025, 0.2718492609857089],
     )
         u = @SVector [0.0, 1000.0, deg2rad(30), 0.0]
         ctf = cunningham_transfer_function(m, u, d, r)
