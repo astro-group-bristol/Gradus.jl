d4469bd6f41b4336558861d7a6ad6e06751b5cd7
fjebaker <fergusbkr@gmail.com>
2023-04-29 23:36:08 +0100

    test: updated test values

--- test/integration/test-precision.jl
+++ test/integration/test-precision.jl
@@ -9,14 +9,14 @@ u = SVector(0.0, 1000.0, π / 2, 0.0)
 target = SVector(10.0, 0.001, 0.0)
 
 α, β, accuracy = Gradus.impact_parameters_for_target(target, m, u)
-@test α ≈ -0.001950232871615813 rtol = 1e-3
+@test α ≈ -0.0023917213583602584 rtol = 1e-3
 @test β ≈ 10.969606493445841 rtol = 1e-3
-@test accuracy ≈ 0.00870713834731178 rtol = 1e-3
+@test accuracy ≈ 0.009064564426582843 rtol = 1e-3
 
 # new target
 target = SVector(10.0, deg2rad(40), -π / 4)
 
 α, β, accuracy = Gradus.impact_parameters_for_target(target, m, u)
-@test α ≈ 4.857623042719467 rtol = 1e-3
-@test β ≈ 8.025434644810364 rtol = 1e-3
-@test accuracy ≈ 0.0021553188931118686 rtol = 1e-3
+@test α ≈ 4.848373364532467 rtol = 1e-3
+@test β ≈ 8.012499652493656 rtol = 1e-3
+@test accuracy ≈ 0.0049975644770909105 rtol = 1e-3
