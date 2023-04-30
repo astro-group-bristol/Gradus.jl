ca479d107b3747d03b1badcc05fa80181d323725
fjebaker <fergusbkr@gmail.com>
2023-04-30 13:25:04 +0100

    test: update transfer function tests

--- test/smoke-tests/cunningham-transfer-functions.jl
+++ test/smoke-tests/cunningham-transfer-functions.jl
@@ -1,37 +1,52 @@
 using Test
 using Gradus
-using StaticArrays
 
-@testset "cunningham-transfer-functions" begin
-    m = KerrMetric(1.0, 0.998)
-    d = GeometricThinDisc(0.0, 400.0, deg2rad(90))
-
-    # test for different angles
-    for (angle, expected) in zip(
-        [3, 35, 74, 85],
-        # values last updated: 24/04/2023
-        [
-            0.24325323183855346,
-            0.21844565584667297,
-            0.11478208978397744,
-            0.06742095227866991,
-        ],
+function test_ctf1(a, angle, rₑ)
+    m = KerrMetric(1.0, a)
+    d = GeometricThinDisc(0.0, Inf, π / 2)
+    x = SVector(0.0, 10_000, deg2rad(angle), 0.0)
+    Gradus.cunningham_transfer_function(
+        m,
+        x,
+        d,
+        rₑ,
+        ;
+        chart = Gradus.chart_for_metric(m, 2 * x[2]),
+        N = 80,
     )
-        u = @SVector [0.0, 1000.0, deg2rad(angle), 0.0]
-        ctf = cunningham_transfer_function(m, u, d, 4.0)
-        s = sum(ctf.f) / length(ctf.f)
-        @test isapprox(s, expected, atol = 1e-3, rtol = 0.0)
-    end
-
-    # different radii
-    for (r, expected) in zip(
-        [4.0, 7.0, 10.0, 15.0],
-        # values last updated: 24/04/2023
-        [0.22097479890817756, 0.2599801835844643, 0.26738568957410025, 0.2718492609857089],
-    )
-        u = @SVector [0.0, 1000.0, deg2rad(30), 0.0]
-        ctf = cunningham_transfer_function(m, u, d, r)
-        s = sum(ctf.f) / length(ctf.f)
-        @test isapprox(s, expected, atol = 1e-2, rtol = 0.0)
-    end
 end
+
+function measure_ctf(ctf)
+    sum(ctf.f .* ctf.g✶) / length(ctf.f)
+end
+
+# test for different angles
+@test measure_ctf(test_ctf1(0.998, 3, 4.0)) ≈ 0.12078230790537134 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 35, 4.0)) ≈ 0.10309564736283175 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 74, 4.0)) ≈ 0.05346607396286273 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 85, 4.0)) ≈ 0.034591443363911956 atol = 1e-5
+
+# different radii
+@test measure_ctf(test_ctf1(0.998, 30, 4.0)) ≈ 0.10702748528655079 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 30, 7.0)) ≈ 0.11941254099997582 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 30, 10.0)) ≈ 0.12411523229602836 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 30, 15.0)) ≈ 0.1269951484304088 atol = 1e-5
+
+# large radii
+@test measure_ctf(test_ctf1(0.998, 30, 300.0)) ≈ 0.134116533963842 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 30, 800.0)) ≈ 0.1339736833006852 atol = 1e-5
+@test measure_ctf(test_ctf1(0.998, 30, 1000.0)) ≈ 0.1359210533432173 atol = 1e-5
+
+# ones that have been problematic in the past
+# and should not raise any errors or warnings
+test_ctf1(-0.6, 88, 784.8253509875607)
+test_ctf1(-0.998, 88, 953.9915665264327)
+test_ctf1(-0.450, 88, 952.1406350219423)
+test_ctf1(0.0, 88, 952.1406350219423)
+test_ctf1(0.0, 88, 631.1007589946363)
+test_ctf1(0.9, 88, 952.1406350219423)
+test_ctf1(0.0, 88, 950.8754196211696)
+test_ctf1(0.744, 88, 3.1880132176627862)
+
+# poor offset radius
+test_ctf1(1.0, 88, 1.01)
