820250397fd061ff6cb528846fb475b37f731536
fjebaker <fergusbkr@gmail.com>
2023-04-30 13:51:48 +0100

    test: update chart test values

--- test/integration/test-charts.jl
+++ test/integration/test-charts.jl
@@ -7,7 +7,7 @@ u = SVector(0.0, 1000.0, π / 2, 0.0)
 
 _, _, img = rendergeodesics(m, u, 2000.0, image_width = 100, image_height = 100, fov = 6.0)
 # default chart
-@test sum(filter(!isnan, img)) ≈ 2.7725698197810156e6 rtol = 0.0001
+@test sum(filter(!isnan, img)) ≈ 2.7734094639084e6 rtol = 0.0001
 
 _, _, img = rendergeodesics(
     m,
@@ -19,4 +19,4 @@ _, _, img = rendergeodesics(
     chart = event_horizon_chart(m, closest_approach = 1.001, resolution = 100),
 )
 # chart that maps event horizon shape better
-@test sum(filter(!isnan, img)) ≈ 2.762717446130311e6 rtol = 0.0001
+@test sum(filter(!isnan, img)) ≈ 2.763547521760295e6 rtol = 0.0001
