8b1a9928985862fee4b220373f46cbd57257ff18
fjebaker <fergusbkr@gmail.com>
2023-05-31 12:58:46 +0100

    feat: use generated functions for geodesic equation
    Results in a 6x speedup in evaluating the geodesic equation once the
    Jacobian and inverse metric has been determined. Lends itself to
    slightly better generics when other metric classes are fully
    implemented.
    
    Uses Julia generated functions to precompute the functional form of the
    matrices, and then uses matrix multiplication to quickly and efficiently
    evaluate the static arrays.
    
    Overall performance boost of a render is approx 10%.

--- src/tracing/method-implementations/auto-diff.jl
+++ src/tracing/method-implementations/auto-diff.jl
@@ -76,7 +76,6 @@ end
 inverse_metric_components(m::AbstractStaticAxisSymmetric, rθ) =
     inverse_metric_components(metric_components(m, rθ))
 
-
 """
     $(TYPEDSIGNATURES)
 
@@ -111,81 +110,48 @@ The Christoffel symbols ``\\Gamma^{\\mu}_{\\phantom{\\mu}\\nu\\sigma}`` are defi
 
 Limitations:
 - currenly pre-supposes static, axis-symmetric metric.
-
-# Notes
-This function is symbolically pre-computed using the following code:
-
-```julia
-using Symbolics, Tullio
-@variables ginv[1:5], j1[1:5], j2[1:5], v[1:4] # non zero metric components
-inverse_metric = [
-    ginv[1] 0 0 ginv[5]
-    0 ginv[2] 0 0
-    0 0 ginv[3] 0
-    ginv[5] 0 0 ginv[4]
-]
-j1_mat = [
-    j1[1] 0 0 j1[5]
-    0 j1[2] 0 0
-    0 0 j1[3] 0
-    j1[5] 0 0 j1[4]
-]
-j2_mat = [
-    j2[1] 0 0 j2[5]
-    0 j2[2] 0 0
-    0 0 j2[3] 0
-    j2[5] 0 0 j2[4]
-]
-j0 = zeros(Float64, (4, 4))
-jacobian = (j0, j1_mat, j2_mat, j0)
-# christoffel symbols
-@tullio Γ[i, k, l] :=
-    1 / 2 *
-    inverse_metric[i, m] *
-    (jacobian[l][m, k] + jacobian[k][m, l] - jacobian[m][k, l])
-# compute geodesic equation
-@tullio δxδλ[i] := -v[j] * Γ[i, j, k] * v[k]
-```
 """
-@muladd @fastmath function compute_geodesic_equation(ginv, j1, j2, v)
-    @inbounds let gi1 = ginv[1],
-        gi2 = ginv[2],
-        gi3 = ginv[3],
-        gi4 = ginv[4],
-        gi5 = ginv[5],
-        j11 = j1[1],
-        j12 = j1[2],
-        j13 = j1[3],
-        j14 = j1[4],
-        j15 = j1[5],
-        j21 = j2[1],
-        j22 = j2[2],
-        j23 = j2[3],
-        j24 = j2[4],
-        j25 = j2[5],
-        v1 = v[1],
-        v2 = v[2],
-        v3 = v[3],
-        v4 = v[4]
+@generated function compute_geodesic_equation(
+    _ginv::SVector,
+    _j1::SVector,
+    _j2::SVector,
+    _v::SVector,
+)
+    Symbolics.@variables ginv[1:5], j1[1:5], j2[1:5], v[1:4] # non zero metric components
+    inverse_metric = [
+        ginv[1] 0 0 ginv[5]
+        0 ginv[2] 0 0
+        0 0 ginv[3] 0
+        ginv[5] 0 0 ginv[4]
+    ]
+    j1_mat = [
+        j1[1] 0 0 j1[5]
+        0 j1[2] 0 0
+        0 0 j1[3] 0
+        j1[5] 0 0 j1[4]
+    ]
+    j2_mat = [
+        j2[1] 0 0 j2[5]
+        0 j2[2] 0 0
+        0 0 j2[3] 0
+        j2[5] 0 0 j2[4]
+    ]
+    j0 = zeros(Symbolics.Num, (4, 4))
+    jacobian = (j0, j1_mat, j2_mat, j0)
+    # christoffel symbols
+    @tullio Γ[i, k, l] :=
+        1 / 2 *
+        inverse_metric[i, m] *
+        (jacobian[l][m, k] + jacobian[k][m, l] - jacobian[m][k, l])
+    quote
+        @inbounds @muladd @fastmath let ginv = _ginv, j1 = _j1, j2 = _j2, v = _v
+            Γ1 = SMatrix{4,4}($(Symbolics.toexpr.(Γ[1, :, :])...))
+            Γ2 = SMatrix{4,4}($(Symbolics.toexpr.(Γ[2, :, :])...))
+            Γ3 = SMatrix{4,4}($(Symbolics.toexpr.(Γ[3, :, :])...))
+            Γ4 = SMatrix{4,4}($(Symbolics.toexpr.(Γ[4, :, :])...))
 
-        SVector{4}(
-            -2(0.5gi5 * j14 + 0.5gi1 * j15) * v2 * v4 -
-            2(0.5gi1 * j11 + 0.5gi5 * j15) * v1 * v2 -
-            2(0.5gi1 * j21 + 0.5gi5 * j25) * v1 * v3 -
-            2(0.5gi5 * j24 + 0.5gi1 * j25) * v3 * v4,
-            0.5(v1^2) * gi2 * j11 +
-            0.5(v3^2) * gi2 * j13 +
-            0.5(v4^2) * gi2 * j14 +
-            gi2 * j15 * v1 * v4 - 0.5(v2^2) * gi2 * j12 - gi2 * j22 * v2 * v3,
-            0.5(v1^2) * gi3 * j21 +
-            0.5(v2^2) * gi3 * j22 +
-            0.5(v4^2) * gi3 * j24 +
-            gi3 * j25 * v1 * v4 - 0.5(v3^2) * gi3 * j23 - gi3 * j13 * v2 * v3,
-            -2(0.5gi4 * j14 + 0.5gi5 * j15) * v2 * v4 -
-            2(0.5gi4 * j24 + 0.5gi5 * j25) * v3 * v4 -
-            2(0.5gi5 * j11 + 0.5gi4 * j15) * v1 * v2 -
-            2(0.5gi4 * j25 + 0.5gi5 * j21) * v1 * v3,
-        )
+            -SVector{4}((Γ1 * v) ⋅ v, (Γ2 * v) ⋅ v, (Γ3 * v) ⋅ v, (Γ4 * v) ⋅ v)
+        end
     end
 end
 
