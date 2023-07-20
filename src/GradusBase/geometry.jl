f08f9e70462abb0ca3ca4feb86c28781e8205d53
fjebaker <fergusbkr@gmail.com>
2023-07-20 09:08:23 +0100

    fix: tetradframe works for LNRF and ZAMO
    Function now correctly orders and reorders both the calls to gramschmidt
    and the returned vectors based on non-zero components of the input
    velocity.
    
    Added doc strings to relevant tetrad functions.
    
    Co-authored-by: wiktoriatarnopolska <wiktoriatarnopolska@gmail.com>

--- src/GradusBase/geometry.jl
+++ src/GradusBase/geometry.jl
@@ -47,25 +47,62 @@ function gramschmidt(v, basis, g; tol = 4eps(eltype(g)))
     v / vnorm
 end
 
-# TODO: this presupposes static and axis symmetric
-# this works for LNRF, but needs to work for e.g. ZAMO as well
-# which means we need to reorder the approach depending on the 
-# non-zero components of `v`
-@inline function tetradframe(g::AbstractMatrix{T}, v) where {T}
-    # normalise the vector to unit length
-    vt = v ./ √abs(propernorm(g, v))
-    # start procedure with ϕ, which has zero for r and θ
-    vϕ = gramschmidt(SVector{4,T}(1, 0, 0, 1), (vt,), g)
-    # then do r, which has zero for θ
-    vr = gramschmidt(SVector{4,T}(1, 1, 0, 1), (vt, vϕ), g)
-    # then finally θ
-    vθ = gramschmidt(SVector{4,T}(1, 1, 1, 1), (vt, vϕ, vr), g)
-    (vt, vr, vθ, vϕ)
-end
+"""
+    _tetrad_permute(x)
 
+Permute the spatial components (vectors) of a given tetrad. That is, given
+some `x = (e1, e2, e3, e4)`, a single call to `_tetrad_permute` will return
+`(e1, e4, e2, e3)`.
+
+Attempts to respect the input type in the return type. Checked for `SVector` and `NTuple`.
+"""
+_tetrad_permute(x::SVector{4,T}) where {T} = SVector{4,T}(x[1], x[4], x[2], x[3])
+_tetrad_permute(x::NTuple{4}) = (x[1], x[4], x[2], x[3])
+
+"""
+    tetradframe(m::AbstractMetric, x, v)
+    tetradframe(g::AbstractMatrix [= metric(m, x)], v)
+
+Compute a tetrad frame via the Gram-Schmidt orthonormalization procedure (see [`gramschmidt`](@ref)),
+where the first vector of the frame is the velocity vector `v` at some position `x`.
+
+Used to compute the locally non-rotating frame via [`lnrframe`](@ref) and [`lnrbasis`](@ref) 
+respectively.
+
+Attempts to reorder the tetrad into a canonical form associated with the global coordinates,
+that is, returns a tuple that corresponds to the ``x^1, x^2, x^3, x^4`` coordinates of the spacetime.
+"""
+@inline function tetradframe(g::AbstractMatrix{T}, v) where {T}
+    # TODO: this presupposes static and axis symmetric
+    # normalise the vector to unit length
+    v1 = v ./ √abs(propernorm(g, v))
+
+    state = v1 .!= 0
+    # ensure there is an initial direction
+    # else just set it to r
+    if sum(state) == 1
+        state = SVector{4,eltype(state)}(1, 0, 0, 1)
+    end
+    # store number of permutations of the space vectors needed to get 
+    # tetrad in the correct order at the end
+    permutations = searchsortedfirst(@views(state[2:end]), 1)
+    v2 = gramschmidt(SVector{4,T}(state), (v1,), g)
+
+    state = state .| _tetrad_permute(state)
+    v3 = gramschmidt(SVector{4,T}(state), (v1, v2), g)
+
+    state = state .| _tetrad_permute(state)
+    v4 = gramschmidt(SVector{4,T}(state), (v1, v2, v3), g)
+
+    # order and return t, r, θ, ϕ
+    ret = (v1, v2, v3, v4)
+    for _ = 2:permutations
+        ret = _tetrad_permute(ret)
+    end
+    ret
+end
 tetradframe(m::AbstractMetric, x, v) = tetradframe(metric(m, x), v)
 
-# TODO: this presupposes static and axis symmetric
 # tetrad with latin indices down: frame
 function lnrframe(g::AbstractMatrix{T}) where {T}
     ω = -g[1, 4] / g[4, 4]
