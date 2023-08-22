e3f5ac5372cd23509b50f163b1e8afe0d1f7b1bb
wiktoriatarnopolska <115220668+wiktoriatarnopolska@users.noreply.github.com>
2023-08-22 15:02:49 +0100

    Update spectra.jl
    cosmetic changes vol2

--- src/corona/spectra.jl
+++ src/corona/spectra.jl
@@ -3,8 +3,8 @@ struct PowerLawSpectrum{T} <: AbstractCoronalSpectrum
     Γ::T
 end
 
-Spectrum for the power law simply returns the photon index 'Γ'. 'I' stores the 'coronal_spectrum' which is a function 
-of 'spectrum' and 'g' since it is defined as 'g^Γ'.
+Spectrum for the power law simply returns the photon index `Γ`. `I` stores the 'coronal_spectrum' which is a function 
+of `spectrum` and `g` since it is defined as `g^Γ`.
 
 """
 struct PowerLawSpectrum{T} <: AbstractCoronalSpectrum
@@ -14,10 +14,10 @@ end
 """
     function coronal_spectrum(spectrum::PowerLawSpectrum, g)
 
-Function takes the PowerLawSpectrum type spectrum (photon index 'Γ') and 'g' as arguments and returns the coronal 
-spectrum as 'g' to the power of 'Γ'. Since the number of photons travelling along any given ray must be conserved 
+Function takes the PowerLawSpectrum type spectrum (photon index `Γ`) and `g` as arguments and returns the coronal 
+spectrum as `g` to the power of `Γ`. Since the number of photons travelling along any given ray must be conserved 
 for different values of the photon index, it serves as a method to accurately account for
-change in the number of photons in each bin of the emissivity profile, with the default value of 'Γ' = 2 [as
+change in the number of photons in each bin of the emissivity profile, with the default value of `Γ` = 2 [as
 per Gonzalez et. al (2017)]
 """
 function coronal_spectrum(spectrum::PowerLawSpectrum, g)
