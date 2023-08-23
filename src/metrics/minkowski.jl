2d22b131deadb32cc43e59622c857f0b88a3e946
fjebaker <fergusbkr@gmail.com>
2023-08-23 23:26:23 +0100

    feat: more minkowski utility structures

--- src/metrics/minkowski.jl
+++ src/metrics/minkowski.jl
@@ -1,6 +1,5 @@
 
-struct SphericalMetric{T} <: AbstractStaticAxisSymmetric{T} end
-
+struct SphericalMetric{T} <: AbstractStaticSphericallySymmetric{T} end
 SphericalMetric() = SphericalMetric{Float64}()
 
 function Gradus.metric_components(::SphericalMetric, rθ)
@@ -13,5 +12,30 @@ function Gradus.metric_components(::SphericalMetric, rθ)
 
     SVector(tt, rr, θθ, ϕϕ, zero(T))
 end
+inner_radius(::SphericalMetric{T}) where {T} = 4eps(T)
 
-inner_radius(::SphericalMetric) = 4eps()
+struct CartesianMetric{T} <: AbstractMetric{T,FlatCartesian{(:x, :y, :z)}} end
+CartesianMetric() = CartesianMetric{Float64}()
+
+function Gradus.metric_components(::CartesianMetric, x)
+    T = eltype(x)
+    SVector{4,T}(-1, 1, 1, 1)
+end
+inner_radius(::CartesianMetric{T}) where {T} = 4eps(T)
+
+# trait based dispatch to get the minkowski metric depending on the coordinates of the spacetime
+minkowski_with_coords(::M) where {M<:AbstractMetric} = minkowski_metric(M)
+minkowski_with_coords(::Type{<:AbstractMetric{T,<:BoyerLindquist}}) where {T} =
+    SphericalMetric{T}()
+minkowski_with_coords(::Type{<:AbstractMetric{T,<:FlatCartesian}}) where {T} =
+    CartesianMetric{T}()
+
+minkowski_matrix(m::AbstractMetric, x) = metric(minkowski_metric(m), x)
+minkowski_matrix() = @SMatrix [
+    -1 0 0 0
+    0 1 0 0
+    0 0 1 0
+    0 0 0 1
+]
+
+export minkowski_matrix
