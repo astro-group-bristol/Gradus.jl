2e8791cec8f4ced21f65556eccad259a6aa9cdb7
fjebaker <fergusbkr@gmail.com>
2023-08-25 22:22:48 +0100

    test(beamedpointsource): reduce n_samples
    100,000 samples takes a very long time on the CI, reduced the number of
    samples down to 100, since we don't need a super accurate result for
    this test.

--- test/beamedpointsource-test.jl
+++ test/beamedpointsource-test.jl
@@ -7,9 +7,9 @@ d = GeometricThinDisc(0.0, 100.0, π / 2)
 model0 = LampPostModel(h = 10.0)
 model1 = Gradus.BeamedPointSource(10.0, 0.0)
 
-em_prof0 = Gradus.emissivity_profile(m, d, model0, n_samples = 100_000)
+em_prof0 = Gradus.emissivity_profile(m, d, model0, n_samples = 100)
 
-em_prof1 = Gradus.emissivity_profile(m, d, model1, n_samples = 100_000)
+em_prof1 = Gradus.emissivity_profile(m, d, model1, n_samples = 100)
 
 profile0 = Gradus.RadialDiscProfile(em_prof0)
 
