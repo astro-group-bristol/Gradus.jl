1ba4e9a85aa885a0aa5f8d281a7920b4baa0a32a
fjebaker <fergusbkr@gmail.com>
2023-09-06 22:29:04 +0100

    test: add test case for using em prof in lineprof

--- test/line-profiles/test-api.jl
+++ test/line-profiles/test-api.jl
@@ -8,7 +8,20 @@ m = KerrMetric(M = 1.0, a = 0.6)
 u = @SVector [0.0, 1000.0, deg2rad(60), 0.0]
 d = GeometricThinDisc(0.0, 250.0, π / 2)
 
-x, y
-
 rtype = returntype(lineprofile, m, u, d)
 @test isconcretetype(rtype) || "$(rtype)"
+
+prof = emissivity_profile(m, d, LampPostModel(), n_samples = 100)
+
+x, y = lineprofile(m, u, d, prof; numrₑ = 3, N = 20, algorithm = CunninghamLineProfile())
+@test sum(y) ≈ 1 atol = 1e-4
+
+x, y = lineprofile(
+    m,
+    u,
+    d,
+    prof;
+    plane = PolarPlane(GeometricGrid(); Nr = 10, Nθ = 10, r_max = 90.0),
+    algorithm = BinnedLineProfile(),
+)
+@test sum(y) ≈ 1 atol = 1e-4
