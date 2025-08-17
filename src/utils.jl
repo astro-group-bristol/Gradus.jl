4138880cfb99e02c144f631808d19ac7247635fe
fjebaker <fergus@cosroe.com>
2025-08-17 22:37:21 +0100

    fix: small changes to the definition of smooth_interpolate

--- src/utils.jl
+++ src/utils.jl
@@ -155,17 +155,18 @@ _rotate_about_spinaxis(n::SVector{3}, ϕ) = SVector(n[1] * cos(ϕ), n[1] * sin(�
 
 _zero_if_nan(x::T) where {T} = isnan(x) ? zero(T) : x
 
-@inline function _smooth_interpolate(
+function _smooth_interpolate(
     x::T,
     x₀;
     δx = T(2.5),
     smoothing_offset = T(1e4),
 ) where {T}
-    if x ≤ x₀
+    if x ≤ (x₀ - δx/2)
         one(T)
-    elseif x₀ ≤ x ≤ x₀ + δx
+    elseif (x₀ - δx/2) ≤ x ≤ (x₀ + δx/2)
         t = (x - x₀) / δx
-        atan(smoothing_offset * t) * 2 / π
+        v = atan(smoothing_offset * t) / π + (1/2)
+        1 - v
     else
         zero(T)
     end
