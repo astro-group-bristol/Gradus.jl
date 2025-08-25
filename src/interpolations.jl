f98a1cd01ee2a22ea8c344cc7dca5ed8ce47b75b
fjebaker <fergus@cosroe.com>
2025-08-25 11:58:54 +0100

    feat: kernel interpolation methods

--- src/interpolations.jl
+++ src/interpolations.jl
@@ -51,3 +51,67 @@ end
     end
     clamp(r, r_min, r_max)
 end
+
+function gaussian_kernel(kernel_size; σ = 1, domain = (-5, 5))
+    function _gaussian(x, y)
+        exp(-((x/σ)^2 + (y/σ)^2))
+    end
+    kernel = zeros(Float64, kernel_size)
+    for (i, xi) in enumerate(range(domain..., kernel_size[1]))
+        for (j, yj) in enumerate(range(domain..., kernel_size[2]))
+            kernel[j, i] = _gaussian(xi, yj)
+        end
+    end
+    kernel ./= sum(kernel)
+    kernel
+end
+
+function constant_kernel(kernel_size)
+    kernel = fill(1.0, kernel_size)
+    kernel ./= sum(kernel)
+    kernel
+end
+
+function kernel_interpolate!(
+    data;
+    kernel_size = (5, 5),
+    kf = gaussian_kernel,
+    verbose = false,
+    kwargs...,
+)
+    output = deepcopy(data)
+    kernel = kf(kernel_size; kwargs...)
+
+    half_x = div(kernel_size[1], 2)
+    half_y = div(kernel_size[2], 2)
+
+    bar = Gradus.init_progress_bar("interpolation", size(output, 1), verbose)
+
+    for i = (half_x+1):(size(output, 1)-half_x)
+        for j = (half_y+1):(size(output, 2)-half_y)
+            v = data[i, j]
+            if isnan(v)
+                slice = @views data[(i-half_x):(i+half_x), ((j-half_y):(j+half_y))]
+                @assert size(kernel) == size(slice) "$(size(kernel)) != $(size(slice))"
+
+                output[i, j] = 0
+                weight = 0.0
+                for (v, k) in zip(slice, kernel)
+                    if !isnan(v)
+                        output[i, j] += v * k
+                        weight += k
+                    end
+                end
+
+                if weight > 0
+                    output[i, j] /= weight
+                end
+            end
+        end
+        Gradus.ProgressMeter.next!(bar)
+    end
+
+    Gradus.ProgressMeter.finish!(bar)
+
+    data .= output
+end
